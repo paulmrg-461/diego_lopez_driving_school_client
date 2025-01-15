848fe0503ade11d93422fac412debba953bb0460
paulmrg-461 <paulmrg461@gmail.com>
2025-01-15 12:52:16 -0500

    Feat: Clean constants

--- lib/presentation/constants/personal_data_processing.dart
+++ lib/presentation/constants/personal_data_processing.dart
@@ -2,27 +2,13 @@
 
 const List<String> dataProcessingPurposesList = [
   'Realizar invitaciones a eventos y ofrecer nuevos productos y servicios.',
-  'Suministrar información de contacto a la fuerza comercial y/o red de distribución, telemercadeo, investigación de mercados y cualquier tercero con el cual CDA PANAMERICANA SAS tenga un vínculo contractual para el desarrollo de actividades de ese tipo.',
-  'Contactar al Titular a través de medios telefónicos, electrónicos, SMS, WhatsApp y redes sociales para el envío de noticias relacionadas con campañas de fidelización o mejora de servicio.',
-  'Gestionar Trámites (solicitudes, quejas, reclamos).',
-  'Contactar al Titular a través de medios telefónicos, electrónicos, SMS, WhatsApp y redes sociales para realizar encuestas, estudios y/o confirmación de datos personales.',
-  'Contactar al Titular a través de medios telefónicos, electrónicos, SMS, WhatsApp y redes sociales para recordar sobre el vencimiento del certificado de RTMyEC y del SOAT.',
 ];
 
 const List<String> dataProcessingRights = [
   'DERECHOS COMO TITULAR DE LOS DATOS PERSONALES CONSIGNADOS - (LEY 1581 DE 2012)',
-  'a. Conocer, actualizar y rectificar sus datos personales frente a los responsables del Tratamiento o Encargados del Tratamiento. Este derecho se podrá ejercer, entre otros frente a datos parciales, inexactos, incompletos, fraccionados, que induzcan a error, o aquellos cuyo Tratamiento esté expresamente prohibido o no haya sido autorizado.',
-  'b. Solicitar copia de esta autorización al CDA PANAMERICANA SAS.',
-  'c. Ser informado por el CDA PANAMERICANA SAS., previa solicitud, respecto del uso que les ha dado a sus datos personales.',
-  'd. Presentar ante la Superintendencia de Industria y Comercio quejas por infracciones a lo dispuesto en la Ley 1581 de 2012 y las demás normas que la modifiquen, adicionen o complementen.',
-  'e. Revocar la autorización y/o solicitar la supresión del dato cuando en el Tratamiento no se respeten los principios, derechos y garantías constitucionales y legales. La revocatoria y/o supresión procederá cuando la Superintendencia de Industria y Comercio haya determinado que en el Tratamiento el CDA PANAMERICANA SAS., ha incurrido en conductas contrarias a la Ley 1581 de 2012 y a la Constitución.',
-  'f. Acceder en forma gratuita a sus datos personales que hayan sido objeto de Tratamiento.',
+  'a. Conocer, actualizar y rectificar sus datos personales',
 ];
 
 const List<String> dataClarenseRights = [
-  'a.  Me han informado y se han cumplido las condiciones contractuales para la prestación del servicio de revisión técnico mecánica y de emisiones contaminantes.',
-  'b. De haber recibido a satisfacción el vehículo inspeccionado',
-  'c. De haber sido informado de los resultados de la inspección.',
-  'd. De haber recibido el Formato Uniforme de Resultados (FUR).',
-  'e. Que se generó a través del RUNT el certificado de Revisión Técnico Mecánica y de Emisiones Contaminantes (RTM y EC) (En caso de ser aprobado).',
+  'a. Me han informado y se han cumplido las condiciones contractuales para la prestación del servicio.',
 ];
